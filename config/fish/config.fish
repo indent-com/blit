7289b6c2e8003cf1d5eb57115abe930a782e6fb9
Pierre Carrier <pierre@indent.com>
2026-04-01 12:12:39 +0000

    Simplify fish greeting to just cat the banner.
    Generated with [Indent](https://indent.com)
    Co-Authored-By: Indent <noreply@indent.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,3 @@
 function fish_greeting
-    set -l cols (tput cols 2>/dev/null; or echo 80)
-    while read -l line
-        set -l len (string length -- $line)
-        set -l pad (math "$cols - $len")
-        if test $pad -gt 0
-            printf '%*s%s\n' $pad '' $line
-        else
-            echo $line
-        end
-    end </etc/blit-banner.txt
+    cat /etc/blit-banner.txt
 end
